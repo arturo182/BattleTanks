c69499ea7fce8ad41c4940c121f2e5803dc503eb
PiXel <pixel3000@o2.pl>
2011-11-09 12:50:53 +0100

    usunalem stara wersje

--- model_jazdy/eksplozja.cpp
+++
@@ -1,5 +0,0 @@
-#include "eksplozja.h"
-
-Eksplozja::Eksplozja()
-{
-}
--- model_jazdy/tekstury.cpp
+++
@@ -1,6 +0,0 @@
-#include "tekstury.h"
-
-Tekstury::Tekstury(QString sciezka, QSize ilosc_klatek):
-	QPixmap(sciezka),
-	ilosc_klatek(ilosc_klatek),
-	rozmiar_klatki(this->size().width() / this->ilosc_klatek.width(), this->size().height() / this->ilosc_klatek.height()){}
